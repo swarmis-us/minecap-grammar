ec68cf5d2d25b971bcb7ad76e9d6c693bb1dc1a6
alexander-kabirov <alexander.kabirov@gmail.com>
2026-02-23 11:25:31 +0100

    fixed syntax

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -9,9 +9,14 @@
 (call func: (identifier) @function)
 (identifier) @variable
 
-; Derivative and magnitude
-(derivative_expr) @operator
-(magnitude_expr) @operator
+; Vector/tuple punctuation
+(tuple "(" @operator)
+(tuple ")" @operator)
+(tuple "," @operator)
+
+; Derivative and magnitude symbols
+(derivative_expr "'" @operator)
+(magnitude_expr "|" @operator "|" @operator)
 
 ; Literals
 (number) @number
